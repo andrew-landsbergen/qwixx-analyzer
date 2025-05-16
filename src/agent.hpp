9a1dda5c78d8b51d3e47a095ba9c0628cdaace6f
alandsbergen <alandsbergen2000@gmail.com>
2025-05-16 10:21:28 -0400

    user can now specify number of simulations and which agents to use

--- src/agent.hpp
+++ src/agent.hpp
@@ -10,8 +10,11 @@ class State;
 class Agent {
 public:
     Agent() : m_position(0) {};
+
+    virtual ~Agent() = default;
     
     virtual std::optional<size_t> make_move(std::span<const Move> moves, const State& state) const = 0;
+
     void set_position(size_t position) {
         m_position = position;
     }
@@ -22,12 +25,14 @@ protected:
 class RandomAgent : public Agent {
 public:
     RandomAgent() : Agent() {};
+
     std::optional<size_t> make_move(std::span<const Move> moves, const State& state) const override;
 };
 
 class GreedyAgent : public Agent {
 public:
     GreedyAgent(int max_skips) : Agent(), m_max_skips(max_skips) {};
+
     std::optional<size_t> make_move(std::span<const Move> moves, const State& state) const override;
 protected:
     int m_max_skips;
--- src/main.cpp
+++ src/main.cpp
@@ -1,42 +1,82 @@
 #include <chrono>
 #include <iostream>
+#include <sstream>
+#include <tuple>
 
 #include "agent.hpp"
 #include "game.hpp"
 
 int main() {
-    auto start = std::chrono::high_resolution_clock::now();
+    const std::vector<std::string> agent_names = {
+        "Random",
+        "Greedy1Skip",
+        "Greedy2Skip",
+        "Greedy3Skip",
+        "Greedy4Skip"
+    };
 
-    int num_random_wins = 0;
-
-    std::vector<std::unique_ptr<Agent>> players;
-    players.push_back(std::make_unique<RandomAgent>());
-    players.push_back(std::make_unique<GreedyAgent>(2));
-
-    std::vector<Agent*> players_raw;
-    for (const auto& player : players) {
-        players_raw.push_back(player.get());
+    std::cout << "\nWelcome to the Qwixx analyzer tool. The available agents are\n\n";
+    for (size_t i = 0; i < agent_names.size(); ++i) {
+        std::cout << i << ": " << agent_names[i] << '\n';
     }
 
-    for (int i = 0; i < 1'000; ++i) {
+    std::cout << '\n' << "Please input the number of simulations, followed by a sequence of 2 to 5 numbers corresponding to the above numbers for each agent. "
+              << "\nExample: 10000 0 1 for 10000 simulations of Random vs. Greedy1Skip.\n\n";
+    
+    // TOOD: validate input
+    std::string line;
+    std::getline(std::cin, line);
+    std::istringstream iss(line);
+
+    int num_simulations = 1;
+    iss >> num_simulations;    
+
+    std::vector<std::tuple<std::unique_ptr<Agent>, std::string>> players;
+    int agent_selection = 0;
+    int num_agents = 0;
+    while (num_agents < 5 && iss >> agent_selection) {
+        if (agent_selection == 0) {
+            players.push_back(std::tuple(std::make_unique<RandomAgent>(), "Random"));
+        }
+        else if (agent_selection >= 1 && agent_selection <= 4) {
+            std::string name = std::string("Greedy") + std::to_string(agent_selection) + std::string("Skip");
+            players.push_back(std::tuple(std::make_unique<GreedyAgent>(agent_selection), name));
+        }
+        else {
+            std::cout << agent_selection << " is not a valid agent number, skipping...\n";
+            continue;
+        }
+        ++num_agents;
+    }
+
+    auto start = std::chrono::high_resolution_clock::now();
+
+    std::vector<Agent*> player_ptrs;
+    for (const auto& player : players) {
+        player_ptrs.push_back(std::get<0>(player).get());
+    }
+
+    int num_p1_wins = 0;
+
+    for (int i = 0; i < num_simulations; ++i) {
         std::cout << "GAME " << (i + 1) << "\n\n";
 
         // TOOD: should consider reusing this object rather than repeatedly calling its constructor
-        Game game = Game(players_raw);
+        Game game = Game(player_ptrs);
         std::unique_ptr<GameData> stats = game.run();
-        std::cout << "Winner: " << (stats.get()->winners[0] == 0 ? "random" : "greedy") << '\n'
-                  << "Random score: " << stats.get()->final_score[0] << '\n'
-                  << "Greedy score: " << stats.get()->final_score[1] << "\n\n";
-                  //<< "Random scorepad:\n" << stats.get()->final_state.get()->scorepads[0] << '\n'
-                  //<< "Greedy scorepad:\n" << stats.get()->final_state.get()->scorepads[1] << '\n';
+
+        // TODO: make this loop over all players, track win counts for each player, and track ties
+        std::cout << "Winner: " << (std::get<1>(players[stats.get()->winners[0]])) << '\n'
+                  << std::get<1>(players[0]) << " score: " << stats.get()->final_score[0] << '\n'
+                  << std::get<1>(players[1]) << " score: " << stats.get()->final_score[1] << "\n\n";
 
         if (stats.get()->winners[0] == 0) {
-            ++num_random_wins;
+            ++num_p1_wins;
         }
     }
 
-    std::cout << "Random wins: " << num_random_wins << '\n'
-              << "Greedy wins: " << (1'000 - num_random_wins) << '\n';
+    std::cout << std::get<1>(players[0]) << " wins: " << num_p1_wins << '\n'
+              << std::get<1>(players[1]) << " wins: " << (num_simulations - num_p1_wins) << "\n\n";
 
     auto end = std::chrono::high_resolution_clock::now();
     std::chrono::duration<double> duration = end - start;
