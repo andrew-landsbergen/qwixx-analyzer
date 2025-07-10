74eec9066143f777b9daf9608896dc0f397393b3
alandsbergen <alandsbergen2000@gmail.com>
2025-07-09 20:46:55 -0400

    regenerated data

--- src/game.cpp
+++ src/game.cpp
@@ -138,8 +138,8 @@ double Game::evaluate_2p() {
         return 0.0;
     }
 
-    const int ramp_start = 7;
-    const int ramp_end = 22;
+    const int ramp_start = 4;
+    const int ramp_end = 14;
     const double range = static_cast<double>(ramp_end - ramp_start + 1);
     if (m_state->turn_count >= ramp_start && m_state->turn_count <= ramp_end) {
         m_score_diff_weight += (0.75 - 0.25) / range;
